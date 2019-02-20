d69413b6394ce0c7cd430df721300433234fb2cb
Amanda Cao <acao004@bolt.cs.ucr.edu>
2019-02-20 11:13:05 -0800

    fixing coloring pt 3

--- driver_state.cpp
+++ driver_state.cpp
@@ -119,7 +119,7 @@ void rasterize_triangle(driver_state& state, const data_geometry* in[3])
 	double alpha = pbc/abc;
 	double beta = apc/abc;
 	double gamma = abp/abc;
-	if (alpha >= 0 && beta >= 0 && gamma >= 0) {
+	if (alpha >= 0 && beta >= 0 && gamma >= 0 && (alpha+beta+gamma) < 1.001) {
 	    int index = px + py * w;
 	    state.image_color[index] = make_pixel(255,255,255);
 	}
