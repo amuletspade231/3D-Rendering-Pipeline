37851a3411c04552847de557a3fdf82dc8e4bcff
Amanda Cao <acao004@bolt.cs.ucr.edu>
2019-03-11 03:51:21 -0700

    implemented clipping

--- driver_state.cpp
+++ driver_state.cpp
@@ -22,8 +22,10 @@ void initialize_render(driver_state& state, int width, int height)
     state.image_depth=0;
 
     state.image_color = new pixel[width * height];
+    state.image_depth = new float[width * height];
     for (int i = 0; i < width * height; ++i) {
 	state.image_color[i] = make_pixel(0, 0, 0);
+	state.image_depth[i] = 1;
     }
 }
 
@@ -38,6 +40,7 @@ void render(driver_state& state, render_type type)
 {
     switch(type) {
 	case render_type::triangle:
+		{
 		for (int i = 0; i < state.num_vertices; i += 3) { // ith triangle
 		    data_geometry** triangle = new data_geometry*[3];
 		    for (int j = 0; j < 3; ++j) { // jth vertex
@@ -51,12 +54,72 @@ void render(driver_state& state, render_type type)
 			}
 			state.vertex_shader((const data_vertex)v, *triangle[j], state.uniform_data);
 		    }
-		    rasterize_triangle(state, (const data_geometry**)triangle);
+
+		    //triangle[0]->gl_Position /= triangle[0]->gl_Position[3];
+		    //triangle[1]->gl_Position /= triangle[1]->gl_Position[3];
+		    //triangle[2]->gl_Position /= triangle[2]->gl_Position[3];
+
+		    //rasterize_triangle(state, (const data_geometry**)triangle);
+		    clip_triangle(state, (const data_geometry**)triangle, 0);
+		}
 		}
 		break;
-	case render_type::indexed: break;
-	case render_type::fan: break;
-	case render_type::strip: break;
+	case render_type::indexed: 
+	{
+	    const data_geometry *out[3];
+	    data_geometry triangle[3];
+	    data_vertex vertices[3];
+
+	    for (int i = 0; i < 3*state.num_triangles; i += 3) {
+		for(int j = 0; j < 3; j++) {
+		   vertices[j].data = &state.vertex_data[state.index_data[i+j] * state.floats_per_vertex];
+		   triangle[j].data = vertices[j].data;
+		   state.vertex_shader(vertices[j], triangle[j], state.uniform_data);
+		   out[j] = &triangle[j];
+		}
+		//rasterize_triangle(state, out);
+		clip_triangle(state, out, 0);
+	    }
+	}
+	break;
+	case render_type::fan: 
+	{
+	    const data_geometry *out[3];
+	    data_geometry triangle[3];
+	    data_vertex vertices[3];
+
+	    for (int i = 0; i < state.num_vertices; ++i) {
+		for (int j = 0; j < 3; ++j) {
+		    int k = i + j;
+		    if (j == 0) { k = 0; }
+		    vertices[j].data = &state.vertex_data[k*state.floats_per_vertex];
+		    triangle[j].data = vertices[j].data;
+		    state.vertex_shader(vertices[j], triangle[j], state.uniform_data);
+		    out[j] = &triangle[j];
+		}
+		//rasterize_triangle(state, out);
+		clip_triangle(state, out, 0);
+	    }
+	}
+	break;
+	case render_type::strip: 
+	{
+	   const data_geometry *out[3];
+	   data_geometry triangle[3];
+	   data_vertex vertices[3];
+
+	   for (int i = 0; i < state.num_vertices-2; ++i) {
+		for (int j = 0; j < 3; ++j) {
+		    vertices[j].data = &state.vertex_data[(i+j)*state.floats_per_vertex];
+		    triangle[j].data = vertices[j].data;
+		    state.vertex_shader(vertices[j], triangle[j], state.uniform_data);
+		    out[j] = &triangle[j];
+		}
+		//rasterize_triangle(state, out);
+		clip_triangle(state, out, 0);
+	   }
+	}
+	break;
 	default: break;
     }
     std::cout<<"TODO: implement rendering for indexed, fan, and strip."<<std::endl;
@@ -69,13 +132,87 @@ void render(driver_state& state, render_type type)
 // simply pass the call on to rasterize_triangle.
 void clip_triangle(driver_state& state, const data_geometry* in[3],int face)
 {
-    if(face==6)
+    //std::cout << "clip\n";
+    if(face == 1)
     {
         rasterize_triangle(state, in);
         return;
     }
-    std::cout<<"TODO: implement clipping. (The current code passes the triangle through without clipping them.)"<<std::endl;
-    clip_triangle(state,in,face+1);
+    //std::cout<<"TODO: implement clipping. (The current code passes the triangle through without clipping them.)"<<std::endl;
+    vec4 a = in[0]->gl_Position;
+    vec4 b = in[1]->gl_Position;
+    vec4 c = in[2]->gl_Position;
+    const data_geometry *in2[3] = {in[0], in[1], in[2]};
+    data_geometry data1[3];
+    data_geometry data2[3];
+    float a1, a2, b1, b2;
+    vec4 p1, p2;
+
+    if (a[2] < -a[3] && b[2] < -b[3] && c[2] < -c[3]) {
+	//std::cout << "not worth\n";
+	return;
+    } else {
+	if (a[2] < -a[3] && b[2] >= -b[3] && c[2] >= -c[3]) {
+	    b1 = (-b[3] - b[2]) / (a[2] + a[3] - b[3] - b[2]);
+	    b2 = (-a[3] - a[2]) / (c[2] + c[3] - a[3] - a[2]);
+	    p1 = b1 * a + (1 - b1) * b;
+	    p2 = b2 * c + (1 - b2) * a;
+
+	    data1[0].data = new float[MAX_FLOATS_PER_VERTEX];
+	    data1[1] = *in[1];
+	    data1[2] = *in[2];
+
+	    for (int i = 0; i < state.floats_per_vertex; ++i) {
+		switch (state.interp_rules[i]) {
+		    case interp_type::flat:
+			data1[0].data[i] = in[0]->data[i];
+			break;
+		    case interp_type::smooth:
+			data1[0].data[i] = b2 * in[2]->data[i] + (1 - b2) * in[0]->data[i];
+			break;
+		    case interp_type::noperspective:
+			a1 = b2 * in[2]->gl_Position[3] / (b2 * in[2]->gl_Position[3] + (1 - b2) * in[0]->gl_Position[3]);
+			data1[0].data[i] = a1 * in[2]->data[i] + (1 - a1) * in[0]->data[i];
+			break;
+		    default:
+			break;
+		}
+	    }
+	    data1[0].gl_Position = p2;
+	    in2[0] = &data1[0];
+	    in2[1] = &data1[1];
+	    in2[2] = &data1[2];
+
+	    clip_triangle(state,in2,face+1);
+
+	    data2[0].data = new float[MAX_FLOATS_PER_VERTEX];
+	    data2[2] = *in[2];
+
+	    for (int i = 0; i < state.floats_per_vertex; ++i) {
+		switch (state.interp_rules[i]) {
+		    case interp_type::flat:
+			data2[0].data[i] = in[0]->data[i];
+			break;
+		    case interp_type::smooth:
+			data2[0].data[i] = b1 * in[0]->data[i] + (1 - b1) * in[1]->data[i];
+			break;
+		    case interp_type::noperspective:
+			a1 = b1 * in[0]->gl_Position[3] / (b1 * in[0]->gl_Position[3] + (1 - b1) * in[1]->gl_Position[3]);
+			data2[0].data[i] = a1 * in[0]->data[i] + (1 - a1) * in[1]->data[i];
+			break;
+		    default:
+			break;
+		}
+	    }
+	    
+	    data2[0].gl_Position = p1;
+	    in2[0] = &data2[0];
+	    in2[1] = &data1[1];
+	    in2[2] = &data1[0];
+	}
+    }
+
+    clip_triangle(state,in2,face+1);
 }
 
 // Rasterize the triangle defined by the three vertices in the "in" array.  This
@@ -83,47 +220,49 @@ void clip_triangle(driver_state& state, const data_geometry* in[3],int face)
 // fragments, calling the fragment shader, and z-buffering.
 void rasterize_triangle(driver_state& state, const data_geometry* in[3])
 {
+    //std::cout << "rasterize\n";
     int w = state.image_width;
     int h = state.image_height;
     int pixCor[3][2];
     for (int index = 0; index < 3; ++index) {
 	auto x = in[index]->gl_Position[0]/in[index]->gl_Position[3];
 	auto y = in[index]->gl_Position[1]/in[index]->gl_Position[3];
-	//std::cout << "x: " << x << " y: " << y << std::endl;
 	pixCor[index][0] = (w/2.0)*x + w/2.0 - 0.5;
 	pixCor[index][1] = (h/2.0)*y + h/2.0 - 0.5;
-	int image_index = pixCor[index][0] + pixCor[index][1] * w;
-//	data_fragment f;
-//	f.data = new float[MAX_FLOATS_PER_VERTEX];
-//	for (int i = 0; i < state.floats_per_vertex; ++i) {
-//	    f.data[i] = in[index]->data[i];
-//	}
-//	data_output o;
-//	state.fragment_shader((const data_fragment)f, o, state.uniform_data);
-//	std::cout
-//	o.output_color *= 225;
-//	state.image_color[image_index] = make_pixel(o.output_color[0],o.output_color[1],o.output_color[2]);
+	//int image_index = pixCor[index][0] + pixCor[index][1] * w;
     }
     
     int ax = pixCor[0][0]; int ay = pixCor[0][1];
     int bx = pixCor[1][0]; int by = pixCor[1][1];
     int cx = pixCor[2][0]; int cy = pixCor[2][1];
-    //std::cout << ax << " " << ay << " " << bx << " " << by << " " << cx << " " << cy << std::endl;
+
+    //float minX = std::min(0, std::min(ax, std::min(bx, cx)));
+    //float maxX = std::max(std::max(ax, std::max(bx, cx)), w);
+    //float minY = std::min(0, std::min(ay, std::min(by, cy)));
+    //float maxY = std::max(std::max(ay, std::max(by, cy)), h);
+
     double abc = 0.5 * ((bx*cy - cx*by) - (ax*cy - cx*ay) + (ax*by - bx*ay));
 
     for (int py = 0; py < h; ++py) {
     for (int px = 0; px < w; ++px) {
+
 	double pbc = 0.5 * ((bx*cy - cx*by) + (by - cy)*px + (cx - bx)*py);
 	double apc = 0.5 * ((cx*ay - ax*cy) + (cy - ay)*px + (ax - cx)*py);
 	double abp = 0.5 * ((ax*by - bx*ay) + (ay - by)*px + (bx - ax)*py);
 	double alpha = pbc/abc;
 	double beta = apc/abc;
 	double gamma = abp/abc;
+
 	if (alpha >= 0 && beta >= 0 && gamma >= 0) {
 	    int index = px + py * w;
 	    auto *data = new float[MAX_FLOATS_PER_VERTEX];
 	    data_fragment f{data};
 	    data_output o;
+	    float depth = alpha * in[0]->gl_Position[2]/in[0]->gl_Position[3] + beta * in[1]->gl_Position[2]/in[1]->gl_Position[3] + gamma * in[2]->gl_Position[2]/in[2]->gl_Position[3];
+
+	    if (depth > state.image_depth[index]) {
+		continue;
+	    }
 	    
 	    for (int i = 0; i < state.floats_per_vertex; ++i) {
 		float k_gour;
@@ -134,9 +273,9 @@ void rasterize_triangle(driver_state& state, const data_geometry* in[3])
 			break;
 		    case interp_type::smooth:
 			k_gour = (alpha/in[0]->gl_Position[3] + beta/in[1]->gl_Position[3] + gamma/in[2]->gl_Position[3]);
-			a_persp = alpha / (k_gour * (in[0]->gl_Position[3]));
-			b_persp = beta / (k_gour * (in[1]->gl_Position[3]));
-			g_persp = gamma / (k_gour * (in[2]->gl_Position[3]));
+			a_persp = alpha / k_gour / in[0]->gl_Position[3];
+			b_persp = beta / k_gour / in[1]->gl_Position[3];
+			g_persp = gamma / k_gour / in[2]->gl_Position[3];
 			f.data[i] = a_persp*in[0]->data[i] + b_persp*in[1]->data[i] + g_persp*in[2]->data[i];
 			break;
 		    case interp_type::noperspective:
@@ -148,13 +287,11 @@ void rasterize_triangle(driver_state& state, const data_geometry* in[3])
 	    }
 	    state.fragment_shader((const data_fragment)f, o, state.uniform_data);
 	    o.output_color *= 255;
-//	    std::cout << o.output_color << std::endl;
 	    state.image_color[index] = make_pixel(o.output_color[0],o.output_color[1],o.output_color[2]);
-	    //state.image_color[index] = make_pixel(255,255,255);
+	    state.image_depth[index] = depth;
 	}
     }
     }
     
-    //std::cout<<"TODO: implement rasterization"<<std::endl;
 }
 
